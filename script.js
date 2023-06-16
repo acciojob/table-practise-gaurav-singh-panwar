b0ef64959c78744a976ea2596845e2fb7405fb92
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-06-16 21:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+table {
+    width: 100%;
+    border-collapse: collapse;
+    background-color: yellow;
+    border: 1px solid black;
+}
+
+thead,
+tbody {
+    padding: 10px;
+    text-align: center;
+    border: 1px solid black;
+}
+
+th,
+td {
+    padding: 10px;
+    text-align: center;
+    border: 1px solid black;
+}
